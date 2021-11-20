2cf32b2cbffdb92c45ccb00ea52399c82acbf38b
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-21 00:22:06 +0100

    fix bug and spelling errors

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisiorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Lanre just learnt about this new to-do app. HE goes to check out its homepage
         self.browser.get('localhost:8000')
@@ -28,13 +33,11 @@ class NewVisiorTest(unittest.TestCase):
         #'1: 'Pray Tahajjud' as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Pray Tahajjud', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Pray Tahajjud')
 
 
         # There is still a text bo inviting him to add another item.He enters 'Do some ironing' (Lanre likes to wear properly-ironed shirts to his day work) 
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Do some ironing')
         # When he hits enter, the page updates, and now te page lists
         #'1: 'Do some ironing' as an item in a to-do list
@@ -42,10 +45,8 @@ class NewVisiorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again and he can now see both of his items on the list
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn('1: Pray Tahajjud', [row.text for row in rows])
-        self.assertIn('2: Do some ironing', [row.text for row in rows])
+        self.check_for_row_in_list_table('!: Pray Tahajjud')
+        self.check_for_row_in_list_table('!: Do some ironing')
         # Lanre wonders whether the site will remember his list. Then he aees that the ssite has generaated a unique url for him
         self.fail('Finish the test!')
 
