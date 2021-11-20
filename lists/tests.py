c95914c776c9d48b818fd0e45c178a45c288fe53
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-20 02:17:39 +0100

    Refactor homepage view to use atemplate

--- lists/tests.py
+++ lists/tests.py
@@ -7,14 +7,11 @@ from django.http import HttpResponse, response
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # # def test_root_url_resolves_to_homepage_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        req = HttpRequest()
-        response = home_page(req)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
